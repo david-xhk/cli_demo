27b0d63e061f62b5413c74270da00c4c311640ca
Han <hk997@live.com>
2018-04-26 20:33:45 +0800

    Tweaked docs.rst

--- demo/options.py
+++ demo/options.py
@@ -19,7 +19,7 @@ class Option(object):
         name (str): The name of the option.
         desc (str): The description of the option that should be printed in :func:`~demo.demo.Demo.print_options`.
         callback (function): The function that the :func:`~demo.options.Option.call` function should wrap.
-        newline (bool): Whether an empty line should be printed before :attr:`~demo.options.Option.callback` is executed.
+        newline (bool): Whether an empty line should be printed before :attr:`~demo.options.Option.callback` is called in the :func:`~demo.options.Option.call` function.
         retry (bool): Whether an input function should be called again once :attr:`~demo.options.Option.callback` has returned.
         lock (bool): Whether the `key` of a trigerring input function should be received by :attr:`~demo.options.Option.callback`.
         args (tuple): The default arguments that should be used to call :attr:`~demo.options.Option.callback` in the :func:`~demo.options.Option.call` function.
