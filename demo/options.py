b9662a6d91d03038b7e2e606b35661e3bb403f24
Han <hk997@live.com>
2018-04-29 16:00:55 +0800

    Tweaked demo docs

--- demo/options.py
+++ demo/options.py
@@ -128,7 +128,7 @@ class DemoOptions(object):
         Returns:
             ``options_decorator()``: A decorator which takes a function (expected to be an input function) and returns a wrapped function.
         
-        The following exceptions will only be raised when the wrapped function is invoked.
+        The following exceptions will only be raised while the wrapped function is being called.
 
         Raises:
             :class:`~demo.exceptions.OptionNotFoundError`: If an option does not exist in :attr:`~demo.options.DemoOptions.registry`, or if its value is not an instance of :class:`~demo.options.Option`.
