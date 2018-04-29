c8bc27550aa6dbce9dd2c073d745d396b0e53d5e
Han <hk997@live.com>
2018-04-30 01:39:24 +0800

    Renamed to 'cli_demo'

--- cli_demo/__init__.py
+++ cli_demo/__init__.py
@@ -4,118 +4,116 @@
 
 Examples:
     Making a simple CodeDemo subclass:
-        In spam.py:
-            ::  
+    ::  
 
-                from cli_demo import CodeDemo
+        from cli_demo import CodeDemo
 
-                class SpamDemo(CodeDemo):
-                    help_text = "An eggs and bacon bonanza."
+        class SpamDemo(CodeDemo):
+            help_text = "An eggs and bacon bonanza."
 
-                    setup_code = '''\\
-                spam = 6
-                eggs = 42'''
+            setup_code = '''\\
+        spam = 6
+        eggs = 42'''
 
-                    commands = [
-                        "spam  # is good for you",
-                        "eggs  # is the meaning of life",
-                        "eggs % spam  # go perfect together",
-                        "response + ' was your response!'"
-                    ]
-    
+            commands = [
+                "spam  # is good for you",
+                "eggs  # is the meaning of life",
+                "eggs % spam  # go perfect together",
+                "response + ' was your response!'"
+            ]
+
     Running a Demo:
-        In a command line interface:
-            ::
+    ::
+
+        >>> from spam import SpamDemo
+        >>> demo = SpamDemo()
+        >>> demo.run()
+        Welcome to SpamDemo!
+
+        Options:
+         *: Any response.
+         h: Help.
+         o: Options.
+         r: Restart.
+         q: Quit.
+
+        Select an option, or type something random: h
+
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        ====
+        Help
+        ====
+
+        SpamDemo
+        --------
+
+        An eggs and bacon bonanza.
+
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        Select an option, or type something random: noodles
+
+        Setup:
+        >>> spam = 6
+        >>> eggs = 42
+
+        Options:
+         0: spam  # is good for you
+         1: eggs  # is the meaning of life
+         2: eggs % spam  # go perfect together
+         3: response + 'was your response!'
+         a: Execute all of the above.
+         c: Setup code.
+         o: Options.
+         r: Restart.
+         q: Quit.
+
+        Choose a command: a
+        >>> spam  # is good for you
+        6
+
+        >>> eggs  # is the meaning of life
+        42
+
+        >>> eggs % spam  # go perfect together
+        0
+
+        >>> response + ' was your response!'
+        'noodles was your response!'
         
-                >>> demo = SpamDemo()
-                >>> demo.run()
-                Welcome to SpamDemo!
+        Choose a command: c
 
-                Options:
-                 *: Any response.
-                 h: Help.
-                 o: Options.
-                 r: Restart.
-                 q: Quit.
+        Setup:
+        >>> spam = 6
+        >>> eggs = 42
 
-                Select an option, or type something random: h
-
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                ====
-                Help
-                ====
-
-                SpamDemo
-                --------
-
-                An eggs and bacon bonanza.
-
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-                Select an option, or type something random: noodles
-
-                Setup:
-                >>> spam = 6
-                >>> eggs = 42
-
-                Options:
-                 0: spam  # is good for you
-                 1: eggs  # is the meaning of life
-                 2: eggs % spam  # go perfect together
-                 3: response + 'was your response!'
-                 a: Execute all of the above.
-                 c: Setup code.
-                 o: Options.
-                 r: Restart.
-                 q: Quit.
-
-                Choose a command: a
-                >>> spam  # is good for you
-                6
-
-                >>> eggs  # is the meaning of life
-                42
-
-                >>> eggs % spam  # go perfect together
-                0
-
-                >>> response + ' was your response!'
-                'noodles was your response!'
-                
-                Choose a command: c
-
-                Setup:
-                >>> spam = 6
-                >>> eggs = 42
-
-                Choose a command: q
-                Goodbye!
+        Choose a command: q
+        Goodbye!
+        
 
     Making a Demo script:
-        1. In spam.py:
-            ::
+    ::
 
-                ...
-                ...
+        ...
+        ...
 
-                if __name__ == "__main__":
-                    demo = SpamDemo()
-                    demo.run()
+        if __name__ == "__main__":
+            demo = SpamDemo()
+            demo.run()
 
-        2. In a command line interface:
-            ::
-            
-                >>> python spam.py
-                Welcome to SpamDemo!
-                ...
-                ...
+    ::
+    
+        >>> python spam.py
+        Welcome to SpamDemo!
+        ...
+        ...
 
-            ::
+    ::
 
-                >>> python3 spam.py
-                Welcome to SpamDemo!
-                ...
-                ...     
+        >>> python3 spam.py
+        Welcome to SpamDemo!
+        ...
+        ...     
 """
 
 __author__ = 'Han Keong'
