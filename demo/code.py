9643fd9c3c8ee496c700983a6e9d18949cbd41cf
Han <hk997@live.com>
2018-04-29 17:20:40 +0800

    Tweaked CodeDemo docs

--- demo/code.py
+++ demo/code.py
@@ -20,7 +20,7 @@ class CodeDemo(Demo):
     Attributes:
         setup_code (str): Code to ``exec`` for setting up a context in :meth:`~demo.code.CodeDemo.setup_callback`.
         command_prompt (str): The input prompt for :meth:`~demo.code.CodeDemo.get_commands`.
-        commands (list[str]): The commands to ``exec`` that the user can choose from in :meth:`~demo.code.CodeDemo.get_commands`.
+        commands (list[str]): The code snippets to ``exec`` that the user can choose from in :meth:`~demo.code.CodeDemo.get_commands`.
         locals (dict): The local namespace for ``exec`` statements. Populated in :meth:`~demo.code.CodeDemo.setup_callback`.
         globals (dict): The global namespace for ``exec`` statements. Populated in :meth:`~demo.code.CodeDemo.setup_callback`.
     """
